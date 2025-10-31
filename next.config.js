ff706f2c574417c63be4bb03edf2c2da0a64b836
Allyssxn <109106722+Allyssxn@users.noreply.github.com>
2025-10-30 23:00:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     cleanDistDir: true,
     images: {unoptimized: true},
     // Set to deploy at GitHub pages or other non-index page url
-    basePath: 'https://allyssxn.site',
+    basePath: '',
     // Required to compile vCard
     webpack(config) {
         config.resolve.fallback = {
